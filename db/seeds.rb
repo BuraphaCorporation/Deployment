bca0012e32bb8e0745a46f9a9b6c9a0a2a25b687
Arnon Hongklay <arnon@hongklay.com>
2016-05-20 15:36:22 +0700

    fix signed in

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,10 @@
   Role.find_or_create_by({name: role})
 end
 
-User.create(
-  email:                 "arnon@hongklay.com",
-  password:              "1234567890",
-  password_confirmation: "1234567890"
-)
+User.first_or_create do |user|
+  user.email                 = 'arnon@hongklay.com',
+  user.password              = '1234567890',
+  user.password_confirmation = '1234567890',
+  user.confirmed_at          = Tome.zone.now
+end
 

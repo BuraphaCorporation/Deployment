739f22f07a4dd19fe3a1c6c046b011665c417ff7
Arnon Hongklay <arnon@hongklay.com>
2016-06-10 13:32:04 +0700

    update daydash

---
+++ ios/client/DaydashTests/DaydashTests.swift
@@ -0,0 +1,36 @@
+//
+//  DaydashTests.swift
+//  DaydashTests
+//
+//  Created by Skilllane on 6/10/2559 BE.
+//  Copyright © 2559 daydash.co. All rights reserved.
+//
+
+import XCTest
+@testable import Daydash
+
+class DaydashTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

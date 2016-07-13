787532bc22c53b93d35384b6d3c5a2ec299c26a8
Arnon Hongklay <arnon@hongklay.com>
2016-07-13 15:22:44 +0700

    fix secret key

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,8 @@ set :deploy_to, '/home/deploy/daydash'
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-set :branch, ENV['BRANCH'] || "master"
+set :branch, fetch(:branch, "develop")
+
 
 namespace :deploy do
 

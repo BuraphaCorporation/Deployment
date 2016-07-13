cffa0f340965840fe4d00891b0cfd9c45ace0b00
Arnon Hongklay <arnon@hongklay.com>
2016-07-13 12:45:13 +0700

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,3 +18,15 @@ namespace :deploy do
   after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
 end
+
+namespace :bower do
+  desc 'Install bower'
+  task :install do
+    on roles(:web) do
+      within release_path do
+        execute :rake, 'bower:install CI=true'
+      end
+    end
+  end
+end
+before 'deploy:compile_assets', 'bower:install'

d0ca2dab18493e6e0ecec9242918f8ee43e1dbfa
Arnon Hongklay <arnon@hongklay.com>
2016-07-14 15:07:11 +0700

    move to daydash.co

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,7 +1,7 @@
 
 GrapeSwaggerRails.options.url = "/api/mobile"
 if Rails.env.production?
-  GrapeSwaggerRails.options.app_url = "https://api.daydash.co"
+  GrapeSwaggerRails.options.app_url = "https://daydash.co"
 else
   GrapeSwaggerRails.options.app_url = "http://localhost:3000"
 end

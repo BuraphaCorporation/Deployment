7112b1e3cb29ad6d72119b2e0e3eb50847764716
Arnon Hongklay <arnon@hongklay.com>
2016-07-16 15:15:16 +0700

    test event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
 
-  has_attached_file :cover, styles: { full: "1440x500#", normal2x: "750x590#", normal3x: "1125x885#", thumb2x: "750x450#", thumb3x: "1125x675#", thumb: '400x500#'  },
+  has_attached_file :cover, styles: { full: "1600x555#", normal2x: "750x590#", normal3x: "1125x885#", thumb2x: "750x450#", thumb3x: "1125x675#", thumb: '800x500#'  },
                     default_url: "/images/:style/missing.png"
   validates_attachment_content_type :cover,
                                     content_type: /\Aimage\/.*\Z/

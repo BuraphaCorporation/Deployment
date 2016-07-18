d4db8fe5d2e014f9bd08ff119722cf022f5f63df
Arnon Hongklay <arnon@hongklay.com>
2016-07-18 15:18:16 +0700

    fix size gallery

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,9 @@ class Gallery < ApplicationRecord
   belongs_to :event
 
   has_attached_file :media,
-   styles: { medium: "500x300>", thumb: "100x50#" },
-   path: ":rails_root/public/images/:id/:filename",
-   url: "/images/:id/:filename"
+    styles: { full: "1600x555#", normal2x: "750x590#", normal3x: "1125x885#", thumb2x: "750x450#", thumb3x: "1125x675#", thumb: '800x500#', facebook: "1200x630" },
+    path: ":rails_root/public/images/:id/:filename",
+    url: "/images/:id/:filename"
 
   do_not_validate_attachment_file_type :media
 end

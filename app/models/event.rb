ec2b84a83176037eb0619d7e8e1a0207eea98019
Arnon Hongklay <arnon@hongklay.com>
2016-07-20 14:25:20 +0700

    fix upload file cover event

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,11 +5,11 @@ class Event < ActiveRecord::Base
   has_many :tickets, dependent: :destroy
   has_many :wishlists, dependent: :destroy
 
-  has_attached_file :cover,
-    styles: { full: "1600x555#", normal2x: "750x590#", normal3x: "1125x885#", thumb2x: "750x450#", thumb3x: "1125x675#", thumb: '800x500#', facebook: "1200x630" },
-    default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :cover,
-                                    content_type: /\Aimage\/.*\Z/
+  # has_attached_file :cover,
+  #   styles: { full: "1600x555#", normal2x: "750x590#", normal3x: "1125x885#", thumb2x: "750x450#", thumb3x: "1125x675#", thumb: '800x500#', facebook: "1200x630" },
+  #   default_url: "/images/:style/missing.png"
+  # validates_attachment_content_type :cover,
+  #                                   content_type: /\Aimage\/.*\Z/
 
   attr_accessor :ticket_name, :ticket_price, :ticket_date, :ticket_time
 

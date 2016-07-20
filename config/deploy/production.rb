3290c1e3cac1b836bb934ca6f9dd1457ead641c4
Arnon Hongklay <arnon@hongklay.com>
2016-07-20 09:58:22 +0700

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,19 @@
 set :stage, :production
 
 # server 'daydash.co', user: 'deploy', roles: %w{web app}
-server 'brick.daydash.co', user: 'deploy', roles: %w{web app}
+server 'daydash.co', user: 'deploy', roles: %w{web app}
+
+# Bonus! Colors are pretty!
+def red(str)
+  "\e[31m#{str}\e[0m"
+end
+
+# Figure out the name of the current local branch
+def current_git_branch
+  branch = `git symbolic-ref HEAD 2> /dev/null`.strip.gsub(/^refs\/heads\//, '')
+  puts "Deploying branch #{red branch}"
+  branch
+end
+
+# Set the deploy branch to the current branch
+set :branch, current_git_branch

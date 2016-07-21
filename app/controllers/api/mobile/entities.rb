9a31f7edbe0ca0b48dd6472d0010c3283b6fc393
Arnon Hongklay <arnon@hongklay.com>
2016-07-21 16:21:59 +0700

    brick

--- app/controllers/api/mobile/entities.rb
+++ app/controllers/api/mobile/entities.rb
@@ -22,7 +22,7 @@ module API
 
       class GalleryExpose < Grape::Entity
         expose :media, as: :src do |item, options|
-          "#{item.media(:thumb)}"
+          "http://brick.daydash.co/#{item.media(:thumb)}"
         end
       end
 

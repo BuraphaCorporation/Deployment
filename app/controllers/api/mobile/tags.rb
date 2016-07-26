3b97e683ecda055d10fee9d6d48ab78ada3b5a5d
Arnon Hongklay <arnon@hongklay.com>
2016-07-26 14:23:18 +0700

    tags

--- app/controllers/api/mobile/tags.rb
+++ app/controllers/api/mobile/tags.rb
@@ -5,13 +5,12 @@ module API
       include API::Mobile::Defaults
 
       resources :tags do
-        desc "Return all events"
+        desc "Return all tags"
         get "/" do
           present :status, :success
-          present :data, Tag.all
+          present :data, Tag.all, with: API::Mobile::Entities::TagExpose
         end
       end
     end
   end
 end
-

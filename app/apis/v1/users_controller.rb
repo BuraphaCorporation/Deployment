214ddc6791c0f01d06716d913b941417cd5f8ee6
Arnon Hongklay <arnon@hongklay.com>
2016-07-27 08:28:55 +0700

    api v1

--- app/apis/v1/users_controller.rb
+++ app/apis/v1/users_controller.rb
@@ -1,13 +1,13 @@
-
-module Api::V1
-  class UsersController < ApiController
-
-    def index
-      render json: User.all
-    end
-
-    def show
-      render json: User.find(params[:id])
-    end
-  end
-end
+#
+# module Api::V1
+#   class UsersController < ApiController
+#
+#     def index
+#       render json: User.all
+#     end
+#
+#     def show
+#       render json: User.find(params[:id])
+#     end
+#   end
+# end

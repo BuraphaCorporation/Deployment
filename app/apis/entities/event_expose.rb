91c76681e3b77db1e0be8b8d3ec50c6b3308b38c
Arnon Hongklay <arnon@hongklay.com>
2016-08-04 14:29:13 +0700

    Api event location_name and location_address

--- app/apis/entities/event_expose.rb
+++ app/apis/entities/event_expose.rb
@@ -2,7 +2,10 @@ class Entities::EventExpose < Grape::Entity
   expose :id
   expose :title
   expose :description
-  expose :location
+  expose :location, as: :location_name
+  expose :location_address do |item, options|
+    "address ยังไม่มีครับ"
+  end
   expose :latitude
   expose :longitude
   expose :categories, using: Entities::CategoryExpose

a35a4b4a12dc526c6eb7fb41473a06451964141b
Arnon Hongklay <arnon@hongklay.com>
2016-08-06 18:52:48 +0700

    user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,8 +20,7 @@ class User < ActiveRecord::Base
 
   enum gender: { male: true, female: false }
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100#" },
-                    default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100#" }
   validates_attachment_content_type :avatar,
                                     content_type: /\Aimage\/.*\Z/
 

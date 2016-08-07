97fec6024874814bd9f7d9031db718777aab3c57
Arnon Hongklay <arnon@hongklay.com>
2016-08-08 01:49:56 +0700

    remove name from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160806023432) do
+ActiveRecord::Schema.define(version: 20160807174625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 20160806023432) do
     t.datetime "created_at",                           null: false
     t.datetime "updated_at",                           null: false
     t.string   "slug"
-    t.string   "name"
     t.index ["slug"], name: "index_events_on_slug", unique: true, using: :btree
     t.index ["user_id"], name: "index_events_on_user_id", using: :btree
   end

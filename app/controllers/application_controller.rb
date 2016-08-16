c437e3e8e9a3d2cdd89f238fe0d5780acc615efb
Arnon Hongklay <arnon@hongklay.com>
2016-08-16 22:56:11 +0700

    check intercom

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  skip_after_action :intercom_rails_auto_include
 
   protected
     def after_sign_in_path_for(resource)

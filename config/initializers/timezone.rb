59c8e4dc38149385578a277224186fd08b3d03a9
Arnon Hongklay <arnon@hongklay.com>
2016-08-18 05:08:07 +0700

    fix timezone

--- config/initializers/timezone.rb
+++
@@ -1,4 +0,0 @@
-
-Rails.application.config.time_zone = 'Bangkok'
-Rails.application.config.active_record.time_zone_aware_types = [:datetime, :time]
-Rails.application.config.active_record.default_timezone = 'Bangkok' #:local

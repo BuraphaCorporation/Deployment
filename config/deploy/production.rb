09f9e618b919d032ec6016c5470adc2e59cfa2ce
Arnon Hongklay <arnon@hongklay.com>
2016-08-18 21:07:08 +0700

    deploy production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server 'beta.daydash.co', user: 'deploy', roles: %w{web app db}
+server 'daydash.co', user: 'deploy', roles: %w{web app db}
 set :stage, :production
 set :rails_env, :production
 set :branch, git_branch('master')

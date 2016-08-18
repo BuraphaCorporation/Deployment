615b428e09654b9ddbe087fb29868ba75afa4f1d
Arnon Hongklay <arnon@hongklay.com>
2016-08-18 16:52:12 +0700

    templates mailer

--- spec/mailers/previews/payment_preview.rb
+++ spec/mailers/previews/payment_preview.rb
@@ -1,8 +1,13 @@
 # Preview all emails at http://localhost:3000/rails/mailers/payment_mailer
-class PaymentMailerPreview < ActionMailer::Preview
+class PaymentPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/payment_mailer/checkout
-  def checkout
-    PaymentMailer.checkout
+  def order
+    PaymentMailer.order(User.first)
   end
+
+  def ticket
+    PaymentMailer.ticket(User.first)
+  end
+
 end

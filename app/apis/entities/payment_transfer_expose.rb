69436f82cc54d5b3369f28a8a96e25e152411ad3
Arnon Hongklay <arnon@hongklay.com>
2016-08-29 17:21:35 +0700

    utc iso8601

--- app/apis/entities/payment_transfer_expose.rb
+++ app/apis/entities/payment_transfer_expose.rb
@@ -6,6 +6,6 @@ class Entities::PaymentTransferExpose < Grape::Entity
   expose :user,     using: Entities::TicketDetailUserExpose
   expose :tickets,  using: Entities::PaymentSectionExpose
   expose :expired_time do |item, option|
-    item.created_at + 1.hours
+    item.created_at.utc.iso8601 + 1.hours
   end
 end

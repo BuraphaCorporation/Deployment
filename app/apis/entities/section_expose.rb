278ab21c0aca3ef2f07b606e6c454cb6b08c71fc
Arnon Hongklay <arnon@hongklay.com>
2016-08-29 21:26:17 +0700

    reverse time

--- app/apis/entities/section_expose.rb
+++ app/apis/entities/section_expose.rb
@@ -2,10 +2,6 @@ class Entities::SectionExpose < Grape::Entity
   expose :id
   expose :title
   expose :price
-  expose :event_time do |item|
-    item.event_time.utc.iso8601
-  end
-  expose :end_time do |item|
-    item.end_time.utc.iso8601
-  end
+  expose :event_time
+  expose :end_time
 end

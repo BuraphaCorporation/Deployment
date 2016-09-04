90c6469cf7bb40d195d8325504617c23bf480f90
Arnon Hongklay <arnon@hongklay.com>
2016-09-05 00:27:26 +0700

    ### Fix spec
    Why:
    
    * ...
    
    This change addresses the need by:
    
    * ...

--- app/mailers/organizer_mailer.rb
+++ app/mailers/organizer_mailer.rb
@@ -1,18 +1,19 @@
 class OrganizerMailer < ApplicationMailer
-  default :template_path => 'layouts_mailer/payment'
+  default :template_path => 'layouts_mailer/organizer'
+
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.organizer_mailer.order.subject
   #
-  def order
+  def order(order)
     @order    = order
     @user     = @order.user
     @event    = @order.event
     @payment  = @order.payment
     @tickets  = @order.tickets
 
-    mail to: @user.email
+    mail to: @event.user.email, cc: "hello@daydash.co"
   end
 end
 

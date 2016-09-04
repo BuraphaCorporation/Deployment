78bf61171f4b219b276f0658c293098f6d18d91f
Arnon Hongklay <arnon@hongklay.com>
2016-09-05 00:37:49 +0700

    fix organizer preview

--- spec/mailers/previews/organizer_preview.rb
+++ spec/mailers/previews/organizer_preview.rb
@@ -3,7 +3,7 @@ class OrganizerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/organizer/order
   def order
-    order = Order.last
+    order = Order.first
     OrganizerMailer.order(order)
   end
 end

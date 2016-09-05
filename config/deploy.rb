16bee7e70fd0bb0b4d712bb484e4e0032cf02714
Arnon Hongklay <arnon@hongklay.com>
2016-09-05 12:39:19 +0700

    ### save
    Why:
    
    * ...

--- config/deploy.rb
+++ config/deploy.rb
@@ -83,19 +83,19 @@ namespace :rails do
 end
 
 
-namespace :sidekiq do
-  task :quiet do
-    on roles(:app) do
-      puts capture("pgrep -f 'workers' | xargs kill -USR1")
-    end
-  end
-  task :restart do
-    on roles(:app) do
-      execute :sudo, :initctl, :restart, :workers
-    end
-  end
-end
+# namespace :sidekiq do
+#   task :quiet do
+#     on roles(:app) do
+#       puts capture("pgrep -f 'workers' | xargs kill -USR1")
+#     end
+#   end
+#   task :restart do
+#     on roles(:app) do
+#       execute :sudo, :initctl, :restart, :workers
+#     end
+#   end
+# end
 
-after 'deploy:starting', 'sidekiq:quiet'
-after 'deploy:reverted', 'sidekiq:restart'
-after 'deploy:published', 'sidekiq:restart'
+# after 'deploy:starting', 'sidekiq:quiet'
+# after 'deploy:reverted', 'sidekiq:restart'
+# after 'deploy:published', 'sidekiq:restart'

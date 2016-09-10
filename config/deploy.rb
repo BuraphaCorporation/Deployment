7b1cd1a623c723ac65532416044a1e89d5bfa2b5
Arnon Hongklay <arnon@hongklay.com>
2016-09-10 17:23:13 +0700

    ### fix deployment
    - What I did
    I've …
    
    - How I did it
    I've changed …
    
    - Description for the changelog
    …….

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,10 +20,10 @@ set :repo_url, 'git@github.com:hongklay/daydash.git'
 set :deploy_to, '/home/deploy/daydash'
 set :ssh_options, {:forward_agent => true}
 
-set :slackistrano, {
-  channel: '#system',
-  webhook: 'https://hooks.slack.com/services/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'
-}
+# set :slackistrano, {
+#   channel: '#system',
+#   webhook: 'https://hooks.slack.com/services/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'
+# }
 
 # set :format, :pretty
 # set :log_level, :debug
@@ -97,7 +97,7 @@ namespace :rails do
   task :logs do
     on roles(:web) do
       within current_path do
-        execute :tail, '-f log/puma_error.log'
+        execute :sudo, 'tail -f /var/log/nginx/error.log'
       end
     end
   end

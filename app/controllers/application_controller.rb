25734089221705fc4ca01253fe8a413db8109757
Arnon Hongklay <arnon@hongklay.com>
2016-09-20 10:34:55 +0700

    ### fix after signup
    - What I did
    I've …
    
    - How I did it
    I've changed …
    
    - Description for the changelog
    …….

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,13 +12,13 @@ protected
   end
 
   def after_sign_in_path_for(resource)
-    if resource.sign_in_count <= 1
-      # welcome_path
-      root_path
-    else
-      # root_path
-      request.env['omniauth.origin'] || stored_location_for(resource) || root_path
-    end
+    # if resource.sign_in_count <= 1
+    #   # welcome_path
+    #   root_path
+    # else
+    #   # root_path
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    # end
   end
 
   def global_categories

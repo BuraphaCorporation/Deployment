1578ad38ace6a6ae1313b5037edc7a6af5caf4ed
Arnon Hongklay <arnon@hongklay.com>
2016-10-11 13:38:44 +0700

    fix wording
    - What I did
    I've …
    
    - How I did it
    I've changed …
    
    - Description for the changelog
    …….

--- app/controllers/client/events_controller.rb
+++ app/controllers/client/events_controller.rb
@@ -153,7 +153,7 @@ class Client::EventsController < Client::CoreController
         UserOrderWorker.perform_async(@order.id)
       end
       # UserTicketWorker.perform_async(@order.id) if @order.payment.status.success?
-      $slack.ping "#{@order.user.try(:first_name)} ได้ทำการ Order ##{@order.code}"
+      $slack.ping "@channel Order ##{@order.code} #{@order.event.try(:title)}\n  #{@order.user.try(:first_name)} #{@order.user.try(:last_name)} เบอร์โทร #{@order.user.try(:phone)}"
     end
 
     render :checkout

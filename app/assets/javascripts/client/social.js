dba3e60ca2e632e40360e1c059c1ba9ec01dbf6f
Arnon Hongklay <arnon@hongklay.com>
2016-10-24 00:57:29 +0700

    fix social error
    ———
    
    - How I did it
    I've changed …
    
    - Description for the changelog
    …….

--- app/assets/javascripts/client/social.js
+++ app/assets/javascripts/client/social.js
@@ -1,6 +1,57 @@
+// var bindFacebookEvents, initializeFacebookSDK, loadFacebookSDK, restoreFacebookRoot, saveFacebookRoot;
+//
+// $(document).on('turbolinks:ready', function() {
+//   loadFacebookSDK();
+//   if (!window.fbEventsBound) {
+//     return bindFacebookEvents();
+//   }
+// });
+//
+// bindFacebookEvents = function() {
+//   $(document).on('turbolinks:fetch', saveFacebookRoot).on('turbolinks:change', restoreFacebookRoot).on('turbolinks:load', function() {
+//     console.log("load");
+//     return typeof FB !== "undefined" && FB !== null ? FB.XFBML.parse() : void 0;
+//   });
+//   return this.fbEventsBound = true;
+// };
+//
+// saveFacebookRoot = function() {
+//   console.log("saveFacebookRoot");
+//   if ($('#fb-root').length) {
+//     return this.fbRoot = $('#fb-root').detach();
+//   }
+// };
+//
+// restoreFacebookRoot = function() {
+//   console.log("restoreFacebookRoot");
+//   if (this.fbRoot != null) {
+//     if ($('#fb-root').length) {
+//       return $('#fb-root').replaceWith(this.fbRoot);
+//     } else {
+//       return $('body').append(this.fbRoot);
+//     }
+//   }
+// };
+//
+// loadFacebookSDK = function() {
+//   console.log("loadFacebookSDK");
+//   window.fbAsyncInit = initializeFacebookSDK;
+//   return $.getScript("//connect.facebook.net/en_US/sdk.js");
+// };
+//
+// initializeFacebookSDK = function() {
+//   return FB.init({
+//     appId: '259929777688738',
+//     version: 'v2.7',
+//     status: true,
+//     cookie: true,
+//     xfbml: true
+//   });
+// };
+
 var bindFacebookEvents, initializeFacebookSDK, loadFacebookSDK, restoreFacebookRoot, saveFacebookRoot;
 
-$(document).on('turbolinks:load', function() {
+$(function() {
   loadFacebookSDK();
   if (!window.fbEventsBound) {
     return bindFacebookEvents();
@@ -8,22 +59,19 @@ $(document).on('turbolinks:load', function() {
 });
 
 bindFacebookEvents = function() {
-  $(document).on('turbolinks:fetch', saveFacebookRoot).on('turbolinks:change', restoreFacebookRoot).on('turbolinks:load', function() {
-    console.log("load");
+  $(document).on('page:fetch', saveFacebookRoot).on('page:change', restoreFacebookRoot).on('page:load', function() {
     return typeof FB !== "undefined" && FB !== null ? FB.XFBML.parse() : void 0;
   });
   return this.fbEventsBound = true;
 };
 
 saveFacebookRoot = function() {
-  console.log("saveFacebookRoot");
   if ($('#fb-root').length) {
     return this.fbRoot = $('#fb-root').detach();
   }
 };
 
 restoreFacebookRoot = function() {
-  console.log("restoreFacebookRoot");
   if (this.fbRoot != null) {
     if ($('#fb-root').length) {
       return $('#fb-root').replaceWith(this.fbRoot);
@@ -34,7 +82,6 @@ restoreFacebookRoot = function() {
 };
 
 loadFacebookSDK = function() {
-  console.log("loadFacebookSDK");
   window.fbAsyncInit = initializeFacebookSDK;
   return $.getScript("//connect.facebook.net/en_US/sdk.js");
 };
@@ -42,9 +89,9 @@ loadFacebookSDK = function() {
 initializeFacebookSDK = function() {
   return FB.init({
     appId: '259929777688738',
-    version: 'v2.7',
     status: true,
     cookie: true,
-    xfbml: true
+    xfbml: true,
+    version: 'v2.7'
   });
 };

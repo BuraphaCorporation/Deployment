5842393ed3ecd0aedf8ee34d08ed17d8f771f600
Arnon Hongklay <arnon@hongklay.com>
2016-10-26 17:06:31 +0700

    fix reminder
    ———
    
    - How I did it
    I've changed …
    
    - Description for the changelog
    …….

--- app/jobs/reminder_job.rb
+++ app/jobs/reminder_job.rb
@@ -3,7 +3,9 @@ class ReminderJob < ApplicationJob
 
   def perform
     Order.event_today.each do |order|
-      UserReminderWorker.perform_async(order.id)
+      UserReminderWorker.perform_async(order.id) if order.events.ticket_type.general?
     end
+  rescue Exception => e
+    put e
   end
 end

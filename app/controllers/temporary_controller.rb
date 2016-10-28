0e1b589af19bc561555618a780bafe7aefed3150
Arnon Hongklay <arnon@hongklay.com>
2016-10-28 10:36:16 +0700

    save
    ———
    
    - How I did it
    I've changed …
    
    - Description for the changelog
    …….

--- app/controllers/temporary_controller.rb
+++ app/controllers/temporary_controller.rb
@@ -11,7 +11,7 @@ class TemporaryController < ApplicationController
   end
 
   def transactions
-    @orders = Order.order(created_at: :desc, status: :asc)
+    @orders = Order.where.not(status: :unpaid).order(created_at: :desc, status: :asc)
   end
 
   def approving

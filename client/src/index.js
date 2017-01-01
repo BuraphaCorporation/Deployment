4e32fc68c51b6f259401a2c286e3c291664666a0
Arnon Hongklay <arnon@hongklay.com>
2017-01-01 13:09:21 +0700

    refactor YTSearch

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -6,16 +6,24 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyATDxbjB5GlP4IquJmInm_qp7dTIgm93Jo';
 
-YTSearch({ key: API_KEY, term: 'surfboards'}, function(data){
-  console.log(data);
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards'}, function(data){
+      // console.log(data);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));

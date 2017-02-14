2f0048fc3cf62ec61245f0ca5e6065b4c1ca68db
Arnon Hongklay <arnon@hongklay.com>
2017-02-14 16:56:55 +0700

    testing

--- app/controllers/client/frontend_controller.rb
+++ app/controllers/client/frontend_controller.rb
@@ -1,4 +1,9 @@
-class Client::FrontendController < ApplicationController
-  def index
+module Client
+  class FrontendController < ApplicationController
+    layout 'client'
+
+    def index
+      @app_props = Event.all
+    end
   end
 end

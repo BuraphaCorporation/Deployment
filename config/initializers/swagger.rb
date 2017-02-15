99b6523dc569a80711f5a4ecd0b36022abb9fc0c
Arnon Hongklay <arnon@hongklay.com>
2017-02-15 16:15:30 +0700

    api

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,3 +1,6 @@
 GrapeSwaggerRails.options.url     = '/v1'
 GrapeSwaggerRails.options.app_url = App.domain('api')
 GrapeSwaggerRails.options.app_name = 'WadeAlikeAPI'
+
+# GrapeSwaggerRails.options.api_key_name = 'api_token'
+# GrapeSwaggerRails.options.api_key_type = 'query'

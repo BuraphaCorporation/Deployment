9a1be021dd408a781607df5c98a70b70aac0d241
Non Madden <nonmadden@gmail.com>
2021-06-02 19:06:50 +0700

    Antify Platform

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module PaymentPlatform
+module AntifyPlatform
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
